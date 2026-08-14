6d9287dd0a92823cfc0f784e76be156440493256
Kishanfdt <ckishan2005@gmail.com>
2026-08-14 12:09:21 +0530

    Update CORS to allow production Vercel domain

--- vitalai/app/main.py
+++ vitalai/app/main.py
@@ -10,12 +10,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://vital-ai-phi.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(triage.router)
 app.include_router(medications.router)
 app.include_router(documents.router)
